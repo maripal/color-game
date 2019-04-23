843e356b4ae6a9b63b22b81ec6cece9c5931d2c9
Maria Palacios <imarip09@gmail.com>
2019-04-23 16:26:33 -0500

    Added rgb span to html file. Also added js file, and functionality

---
+++ index.js
@@ -0,0 +1,35 @@
+let colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
+];
+
+let squares = document.querySelectorAll('.square');
+
+//To display the rgb color you need to guess
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById('colorDisplay');
+
+colorDisplay.textContent = pickedColor;
+
+//loop through the squares to assign a color to each one.
+for (let i = 0; i < squares.length; i++) {
+  //add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener('click', function() {
+    //grab color of clicked square
+    let clickedColor = this.style.backgroundColor;
+
+    //compare color to pickedColor
+    if (clickedColor === pickedColor) {
+      alert('correct');
+    } else {
+      alert('wrong')
+    }
+  })
+}
