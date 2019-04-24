14b4a7adfe33995e3762517be4b5547ec67908b4
Maria Palacios <imarip09@gmail.com>
2019-04-24 12:25:53 -0500

    added functionality for both easy and hard buttons

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-let colors = generateRandomColors(6);
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
 
 let squares = document.querySelectorAll('.square');
 let messageDisplay = document.querySelector('#message');
 let h1 = document.querySelector('h1');
 let resetButton = document.querySelector('#reset');
+let easyBtn = document.querySelector('#easyBtn');
+let hardBtn = document.querySelector('#hardBtn');
+
+easyBtn.addEventListener('click', function() {
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+
+  numSquares = 3
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  //loop so only the top 3 squares change color
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      //hides bottom 3 squares
+      squares[i].style.display = 'none';
+    }
+  } 
+});
+
+hardBtn.addEventListener('click', function() {
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numSquares = 6
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  //loop so only the top 3 squares change color
+  for (let i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = 'block';
+  } 
+})
 
 resetButton.addEventListener('click', function() {
   //when button is clicked. generate all new colors
-  colors = generateRandomColors(6)
+  colors = generateRandomColors(numSquares)
   //pick new random color from array
   pickedColor = pickColor();
   //change color display to match picked color
