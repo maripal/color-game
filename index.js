e47db848de1bccecf003d5a811c178a646109348
Maria Palacios <imarip09@gmail.com>
2019-04-24 11:32:14 -0500

    added functions to choose random colors and got rid of hard-coded colors array

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
+let colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll('.square');
 let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
 
 //To display the rgb color you need to guess
 let pickedColor = pickColor();
@@ -31,6 +25,7 @@ for (let i = 0; i < squares.length; i++) {
       messageDisplay.textContent = "Correct!";
       //If correct, all squares change to that color. So we call this function w/ the correct color(clickedColor).
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -51,4 +46,31 @@ function changeColors(color) {
 function pickColor() {
   let random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+//function to generate colors
+function generateRandomColors(num) {
+  //make an array
+  let arr = [];
+  //repeat num times
+  for (let i = 0; i < num; i++) {
+    //get random color, using the randomColor function below, and push into array
+    arr.push(randomColor())
+  }
+  //return that array
+  return arr;
+
+}
+
+
+function randomColor() {
+  //pick a 'red' from 0-255
+  let r = Math.floor(Math.random() * 256);
+  //pick a 'green' from 0-255
+  let g = Math.floor(Math.random() * 256);
+  //pick a 'blue' from 0-255
+  let b = Math.floor(Math.random() * 256);
+
+  //here we return the random color, by putting it together in proper rgb color form
+  return `rgb(${r}, ${g}, ${b})`;
 }
